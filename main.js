80894951be10cc5973c2666a37c54558d8dc639b
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-20 14:20:49 +0100

    ADD: price calculation function

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ let surname = document.getElementById("surname");
 let km = document.getElementById("km");
 let age = document.getElementById("select");
 
+function price(x, y) {
+    const pricePerKm = 0.21;
+
+    provisionalPrice = x * pricePerKm;
+
+    (y.value == "minor") ? discount = provisionalPrice * 0.2 : (y.value == "over65") ? discount = provisionalPrice * 0.4 : discount = 0;
+    
+    finalPrice = provisionalPrice - discount
+    return finalPrice
+}
+
