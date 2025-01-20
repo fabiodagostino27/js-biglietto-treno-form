f8f3e32683aeb58b8b51136de0344fd66c3b877e
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-20 15:19:31 +0100

    ADD: ticket in the dom

--- main.js
+++ main.js
@@ -2,15 +2,23 @@ let name = document.getElementById("name");
 let surname = document.getElementById("surname");
 let km = document.getElementById("km");
 let age = document.getElementById("select");
+let submit = document.getElementById("submit");
+let cancel = document.getElementById("cancel");
+let ticket = document.getElementById("ticket")
+
 
 function price(x, y) {
     const pricePerKm = 0.21;
 
-    provisionalPrice = x * pricePerKm;
-
+    provisionalPrice = parseInt(x.value) * pricePerKm;
     (y.value == "minor") ? discount = provisionalPrice * 0.2 : (y.value == "over65") ? discount = provisionalPrice * 0.4 : discount = 0;
     
-    finalPrice = provisionalPrice - discount
+    finalPrice = provisionalPrice - discount;
     return finalPrice
 }
 
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+
+})
