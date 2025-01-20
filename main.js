b999b4824394ec890981f9ce2285cb441f3c1bf2
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-20 12:58:58 +0100

    ADD: variables

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = document.getElementById("name");
+let surname = document.getElementById("surname");
+let km = document.getElementById("km");
+let age = document.getElementById("select");
+
